05f7b9fa81dfcdb54515fb90fffc18988e78baea
polargull <fuwei04ruanjian@gmail.com>
2019-04-06 18:26:16 +0800

    init

---
+++ src/main/java/com/example/textbook/TextbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.textbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextbookApplication.class, args);
+	}
+
+}
