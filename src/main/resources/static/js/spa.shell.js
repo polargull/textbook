a940221e43eca9cd7d4a1413d1085d081a2bce8f
fu.wei <wei.fu@mtime.com>
2019-04-11 16:57:38 +0800

    spa init spa.shell

--- src/main/resources/static/js/spa.shell.js
+++ src/main/resources/static/js/spa.shell.js
@@ -11,9 +11,13 @@
 */
 /*global $, spa */
 
-var spa = (function () {
+spa.shell = (function () {
   var initModule = function ( $container ) {
-    spa.shell.initModule( $container );
+    $container.html(
+      '<h1 style="display:inline-block; margin:25px;">'
+        + 'hello world!'
+      + '</h1>'
+    );
   };
   
   return { initModule: initModule };
